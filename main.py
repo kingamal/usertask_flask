c93f18c5db24dee9acc684403c9fd6d57f87e60f
Kinga <kinga.maleszewska@o2.pl>
2022-01-09 00:09:37 +0100

    file csv is downloading from the page by using flask

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import requests
+from flask import Flask, send_file
+import http.client
 
 
 URL_USERS = "https://jsonplaceholder.typicode.com/users"
@@ -13,3 +15,26 @@ def response(url):
 
 users = response(URL_USERS)
 todos = response(URL_TODOS)
+
+
+app = Flask(__name__)
+conn = http.client.HTTPSConnection("localhost", 8080)
+
+
+@app.route('/')
+def homepage():
+    return f"<h2> Plik dostępny pod adresem " \
+           f"<a href='http://127.0.0.1:8080/app/user_task'> http://127.0.0.1:8080/app/user_task</a></h2>"
+
+
+@app.route('/app/user_task')
+def user_task():
+    response = send_file(
+        path_or_file="file.csv",
+        as_attachment=True,
+    )
+    return response
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
